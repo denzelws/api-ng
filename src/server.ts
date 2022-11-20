82d33f05c9ed42e29caf887279e2de1d4f7ad7f0
DenzelWashington <denzelwashingtonsantana@gmail.com>
2022-11-19 23:34:18 -0300

    Add structure

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config'
+import express from 'express'
+import { routes } from './routes'
+
+const app = express()
+app.use(express.json())
+app.use(routes)
+
+app.listen(process.env.API_PORT, () => console.log(`🚀 Server started on port ${process.env.API_PORT}`))
